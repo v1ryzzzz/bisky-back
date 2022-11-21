6606ee6e265f89cd7922f275daa2697390a687d7
v1ryzzzz <73602697+v1ryzzzz@users.noreply.github.com>
2022-11-21 23:46:27 +0300

    minor fix

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,7 @@ import {AppModule} from "./app.module";
 
 // test commit with jetBrains
 async function start() {
-    const PORT = process.env.PORT || 1488;
+    const PORT = process.env.PORT || 5000;
     const app = await NestFactory.create(AppModule)
     app.enableCors();
     await  app.listen(PORT, () => console.log(`Server started on port = ${PORT}`))
